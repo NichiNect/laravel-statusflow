4fc794f36a4aaf810d2ccf4e731e7938075916c8
NichiNect <yoniwidhi2@gmail.com>
2023-05-30 22:14:59 +0700

    Add: prepare model and migration statusflow

---
+++ app/Models/StatusflowDetail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StatusflowDetail extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'statusflow_id', 'current_status', 'next_status', 'level', 'description'
+    ];
+}
---
+++ database/migrations/2023_05_30_144033_create_statusflow_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Statusflow;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('statusflow_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Statusflow::class)->onDelete('cascade');
+            $table->string('current_status')->nullable();
+            $table->string('next_status');
+            $table->integer('level')->default(1);
+            $table->string('description')->nullable();
+            $table->timestamps();
+
+            $table->unique(['current_status', 'next_status', 'level'], 'status_matcher_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('statusflow_details');
+    }
+};
